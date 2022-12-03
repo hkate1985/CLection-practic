b4009c5c48ad2475178b735af0fe1cd7a63cac37
Hkate <hkate1985@gmail.com>
2022-12-04 00:32:31 +0300

    Task1

---
+++ lecTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Вход 2 числа, проверка, являются ли первое число квадратом второго
+
+int number1=Convert.ToInt32(Console.ReadLine());
+int number2=Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
