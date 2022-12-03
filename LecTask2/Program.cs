e5b6ae5d53363d54a7cdb7bff5e3657c36d6107e
Hkate <hkate1985@gmail.com>
2022-12-04 00:42:28 +0300

    Task2

---
+++ LecTask2/Program.cs
@@ -0,0 +1,36 @@
+﻿///Task2.Выдавать название дня недели по заданному номеру (SwitchCase)
+//3-среда
+//5-пятница
+
+Console.WriteLine("введите день недели");
+int day=Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("No day");
+        break;
+
+
+
+}
