2557c0ee64be62b296fe74a32261111a32e1e00a
Hkate <hkate1985@gmail.com>
2022-12-03 23:35:36 +0300

    Task1

--- lec01/Program.cs
+++ lec01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//task1.Напишите программу, которая на вход принимает число и выдает его квадрат (умножение само на себя)
+
+int number = 0;
+string text = "";
+text = Console.ReadLine();
+number = Convert.ToInt32(text);
+Console.WriteLine("Result:" + number*number);
+
+// int number=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Result:", number*number);
