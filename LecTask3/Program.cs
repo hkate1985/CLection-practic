c60da926d2f8494e81f5efe992d97d63997aacbc
Hkate <hkate1985@gmail.com>
2022-12-04 14:31:40 +0300

    Task3

---
+++ LecTask3/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до */
+/*int N=Convert.ToInt32(Console.ReadLine());
+int M=-N;
+while (M<=N)
+{
+    Console.WriteLine(M++);
+}
+Task2*/
+
+Console.Clear();
+int A=int.Parse(Console.ReadLine());
+int LA = A%10;
+Console.WriteLine(LA);
